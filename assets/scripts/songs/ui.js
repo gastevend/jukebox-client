9c22a0af5e886268f23c7edcb045eb5d45fd7ab8
gastevend <steven.duval@libertymutual.com>
2017-04-27 12:39:17 -0400

    this puts the response from get /songs onto the page
    Used handlebars to display the songs in the html on a successful request.
    they are not formatted yet but are displaying on clicking search

--- assets/scripts/songs/ui.js
+++ assets/scripts/songs/ui.js
@@ -1,7 +1,11 @@
 'use strict'
 
+const showSongsTemplate = require('../templates/songlist.handlebars')
+
 const searchSuccess = data => {
   console.log('searhcing songs is working and data is', data)
+  const showSongsHtml = showSongsTemplate({ songs: data.songs })
+  $('.view').append(showSongsHtml)
 }
 
 const searchFailure = error => {
